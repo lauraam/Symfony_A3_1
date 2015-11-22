3fcaccb602ab943254ace6dc09ee75ea0c661cc4
Laura MICHENEAU <lauraalexandra@hotmail.fr>
2015-11-22 22:21:54 +0100

    Symfony_A3_1

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,7 +16,11 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            new JMS\SerializerBundle\JMSSerializerBundle(),
             new AppBundle\AppBundle(),
+            new FOS\RestBundle\FOSRestBundle(),
+
+
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
